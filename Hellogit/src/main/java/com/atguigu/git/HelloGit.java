62f1e0db74653415ac75046a8322d0ab3c5075a3
sulieyZhang <suliey@foxmail.com>
2021-11-27 02:17:21 +0800

    主干添加2

--- Hellogit/src/main/java/com/atguigu/git/HelloGit.java
+++ Hellogit/src/main/java/com/atguigu/git/HelloGit.java
@@ -9,5 +9,6 @@ public class HelloGit {
         System.out.println("hello git");
         System.out.println("创建分支");
         System.out.println("主干上添加");
+        System.out.println("主干添加2");
     }
 }
