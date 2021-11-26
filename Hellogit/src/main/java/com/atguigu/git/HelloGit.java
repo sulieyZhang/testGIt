dba469f482e2a68bf0dcbd7e0cf355b02fa70800
sulieyZhang <suliey@foxmail.com>
2021-11-27 01:59:30 +0800

    first test

---
+++ Hellogit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author suliey
+ * @create 2021-11-27-1:50
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
