1c41adad64be9c3f7c9bd6d87a69d1e4199ba709
suliey <suliey@foxmail.com>
2021-11-27 02:37:05 +0800

    Update HelloGit.java
    在线更新

--- Hellogit/src/main/java/com/atguigu/git/HelloGit.java
+++ Hellogit/src/main/java/com/atguigu/git/HelloGit.java
@@ -12,5 +12,6 @@ public class HelloGit {
         System.out.println("主干添加2");
         System.out.println("分支添加");
         System.out.println("分支添加2");
+        System.out.println("在线更新");
     }
 }
